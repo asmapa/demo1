8109e15322571b4448642d24fac773e66eeb47f4
asma <asmapa122002@gmail.com>
2023-01-16 10:51:43 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int add(int a,int b);
+int main()
+{
+  int sum=0;
+  cout<<"Hello world";
+  cout<<"welcome asmapa";
+  sum=add(5,4);
+  cout<<"sum:"<<sum;
+  }
+  
+  int add(int a,int b)
+  {
+       return(a+b);
+  }
+  
